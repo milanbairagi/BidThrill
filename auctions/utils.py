d40097a8b821df8434d0350dc794d1fd4f8f8039
Milan Bairagi <nmilan626@gmail.com>
2026-06-19 13:34:35 +0545

    refactor: remove depricated utils

--- auctions/utils.py
+++
@@ -1,8 +0,0 @@
-from .models import Watchlist, Bid
-
-def is_in_watchlist(user, item):
-    inwatchlist = False
-    if Watchlist.objects.filter(user=user, item=item).exists():
-        inwatchlist = True
-    return inwatchlist
-    
