313f5e5b04a1cc2a72919cb404fa7511277d04a3
Milan Bairagi <nmilan626@gmail.com>
2026-06-19 12:06:03 +0545

    fix: update media directory in settings.py

--- commerce/settings.py
+++ commerce/settings.py
@@ -123,5 +123,5 @@ LOGIN_URL = "/login"
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR + '/media/'
 MEDIA_URL = '/media/'
